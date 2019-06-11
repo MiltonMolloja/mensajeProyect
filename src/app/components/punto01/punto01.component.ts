fd3895d07c27e296c097912d034ce535a7365c05
Milton Fernado Molloja <milton.molloja@gmail.com>
2019-06-10 21:22:27 -0300

    Complete

--- src/app/components/punto01/punto01.component.ts
+++ src/app/components/punto01/punto01.component.ts
@@ -28,6 +28,7 @@ export class Punto01Component implements OnInit {
     this.empresas = new Array<Empresa>();
     this.obtenerEmrpesas();
     this.mostrarHistoricos();
+    this.enviarMensaje();
     //console.log(this.empresas);
   }
 
@@ -42,12 +43,6 @@ export class Punto01Component implements OnInit {
         console.log(this.empresas);
       }
     );
-  }
-
-  public enviarMensaje() {
-    this.mensaje.fecha = new Date();
-    this.mensajes.push(this.mensaje);
-    this.mensaje = new Mensaje();
   }
 
   public mostrarHistoricos() {
@@ -80,5 +75,58 @@ export class Punto01Component implements OnInit {
     )
   }
 
+  public enviarMensaje() {
+    this.empresa = new Empresa();
+    this.mensaje = new Mensaje();
+    this.mensaje.fecha = new Date();
+    //this.mensaje.id = 17;
+    this.mensaje.desde = 1555555;
+    this.mensaje.para = 133333;
+    this.mensaje.texto = "HOLA";
+    this.empresa.id = 1;
+    this.empresa.nombre = "Personal";
+    this.empresa.razonSocial = "PersonalSA";
+    this.empresa.urlWebsite = "WWW.PersonalSA";
+    this.mensaje.empresa = this.empresa;
+    this.mensajeService.sendMensaje(this.mensaje)
+      .subscribe(
+        result => {
+          console.log("agregado correctamente.");
+        },
+        error => {
+          alert("Error en el envio.");
+        });
+  }
 
+  public elegirMensaje(mensaje: Mensaje) {
+    //Creo una copia del mensaje recibido como parametro para NO modificarlo
+    //ya que el parametro esta mostrandose por el binding en el datatable
+    this.mensaje = Object.assign(this.mensaje, mensaje);
+    //se asigna a la propiedad mensaje.empresa el correspondiente en el
+    //array de empresas, ya que este array es fuente de datos del <select>
+    this.mensaje.empresa = this.empresas.find(function (item: Empresa) {
+      return item.id === mensaje.empresa.id;
+    });
+  }
+
+  public actualizarMensaje() {
+    //seteo nuevamente la fecha actual para el msj modificado
+    this.mensaje.fecha = new Date();
+    this.mensajeService.modificarMensaje(this.mensaje).subscribe(
+      data => {
+        console.log("modificado correctamente.")
+        //actualizo la tabla de mensajes
+        this.mostrarHistoricos();
+        return true;
+      },
+      error => {
+        console.error("Error updating!");
+        console.log(error);
+        return false;
+      });
+  }
+
+  public cambiarTamTexto(){
+
+  }
 }
--- src/app/services/mensaje.service.ts
+++ src/app/services/mensaje.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { Mensaje } from './../models/mensaje';
 
 @Injectable({
   providedIn: 'root'
@@ -33,6 +34,30 @@ export class MensajeService {
   borrarMensaje(id: number){
     //utilizo el metodo delete de http que es el configurado en el deleteAction de Symfony
     return this._http.delete(('http://localhost:8080/mensajeProyect/public/index.php/mensaje/'+id));
+  }
+
+  public sendMensaje(mensaje){
+    const httpOption = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }
+    let body = JSON.stringify(mensaje);
+    return this._http.post('http://localhost:8080/mensajeProyect/public/index.php/mensaje/new', body,
+    httpOption);
+  }
+
+  modificarMensaje(mensaje:Mensaje){
+    const httpOption = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+
+    let body = JSON.stringify(mensaje);
+    //envio en el body el mensaje transformado en un JSON
+    return this._http.post('http://localhost:8080/mensajeProyect/public/index.php/mensaje/'+mensaje.id+'/edit',
+    body, httpOption);
   }
 
 }
